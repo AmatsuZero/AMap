e199efc2b73ed51bd54ff5f8423dce770801954f
jzh16s@hotmail.com <Ultraman7>
2018-06-25 20:26:04 +0800

    ADD: init example project

---
+++ example/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Button } from 'antd-mobile-rn';
+
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return <Button>Start</Button>;
+  }
+}
+
